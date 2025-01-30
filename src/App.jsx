5efaa0dc8e01c4f09eae28d28239da3571ab8fc3
Joshua Ruffolo <joshua.ruffolo@gmail.com>
2025-01-29 18:07:57 -0600

    Add timeframe select and total sum to Results component

--- src/App.jsx
+++ src/App.jsx
@@ -93,28 +93,42 @@ function Card({ habit, onDelete }) {
 }
 
 function Results({ habits }) {
-  const [period, setPeriod] = useState(12);
+  const [timeframe, setTimeframe] = useState(12);
   const WEEK_TO_MONTH = 13 / 3;
   const WEEK_ADJUST = 365 / 364;
+  let sumTime = 0;
 
-  function totalTime(amount, freq, period) {
-    const minutes = amount * freq * period * WEEK_TO_MONTH * WEEK_ADJUST;
+  function totalTime(amount, freq, timeframe) {
+    const minutes = amount * freq * timeframe * WEEK_TO_MONTH * WEEK_ADJUST;
     return Math.ceil(minutes / 60);
   }
 
   const items = habits.map((habit, i) => {
-    const time = totalTime(habit.amount, habit.frequency, period);
-
+    const time = totalTime(habit.amount, habit.frequency, timeframe);
+    sumTime += time;
     return <li key={i}>{habit.name}: {time} hours</li>
   })
 
   return (
     <>
     <h2>Habit Checkout</h2>
-    <p>Period: {period} months</p>
+    <label htmlFor="timeframe">
+        Timeframe:
+        <select onChange={(e) => setTimeframe(e.target.value)} name="timeframe" id="">
+        <option value="1">1 month</option>
+        <option value="2">2 months</option>
+        <option value="3">3 months</option>
+        <option value="6">6 months</option>
+        <option value="12" selected>1 year</option>
+        <option value="24">2 years</option>
+        <option value="36">3 years</option>
+        <option value="60">5 years</option>
+      </select>
+      </label>
     <ul>
       {items}
     </ul>
+    <p>Total time spent toward your goals: {sumTime} hours!</p>
     </>
   )
 }
